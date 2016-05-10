f1658f0e8ffd737cda232885c0c5a3b7718f0718
BrandenSandahl <branden157@gmail.com>
2016-05-09 23:28:12 -0400

    initial import

---
+++ src/main/java/com/sixtel/SpringSecurityApplication.java
@@ -0,0 +1,29 @@
+package com.sixtel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+public class SpringSecurityApplication {
+
+
+	@RequestMapping("/resource")
+	public Map<String,Object> home() {
+		Map<String,Object> model = new HashMap<String,Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+}
